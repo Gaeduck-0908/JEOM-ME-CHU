945caf8b3534e52f43f4f659d2bb24ea7383103b
KimTaeHan <82009667+Gaeduck-0908@users.noreply.github.com>
2023-10-26 11:02:37 +0900

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,19 @@
+var previousFood = "";
+
 function randomSelect() {
     var input = document.getElementById("foods").value;
     var foodList = input.split(",");
-    var randomFood = foodList[Math.floor(Math.random() * foodList.length)];
+    var randomFood = "";
+
+    do {
+        randomFood = foodList[Math.floor(Math.random() * foodList.length)];
+    } while (randomFood === previousFood && foodList.length > 1);
+    
     var resultDiv = document.getElementById("result");
-    resultDiv.innerHTML = "오늘의 점심 메뉴는 " + randomFood + " 입니다!";
+    resultDiv.innerHTML = "오늘의 메뉴는 " + randomFood + " 입니다!";
     resultDiv.className = '';
     void resultDiv.offsetWidth;
     resultDiv.classList.add('animate__animated', 'animate__bounceInDown');
+    
+    previousFood = randomFood;
 }
